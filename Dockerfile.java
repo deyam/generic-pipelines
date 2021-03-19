1a6938b616a9dc9a0efb5c782f7a098e0261ff33
Deya Motawie <deya787@gmail.com>
2021-03-19 15:58:56 +0000

    changed errors in 04

--- Dockerfile.java
+++ Dockerfile.java
@@ -5,7 +5,7 @@ RUN curl -fsSL get.docker.com | sh
 RUN apt-get update && apt-get install -y jq zip
 
 COPY stages /stages
-docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad6
+#docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad6
 RUN docker buildx create --name simx-builder-multi
 RUN docker buildx use simx-builder-multi
 RUN docker buildx inspect --bootstrap
--- stages/04_archive.sh
+++ stages/04_archive.sh
@@ -11,10 +11,10 @@ docker login \
     -u "${DOCKER_USERNAME?:}" \
     -p "${DOCKER_PASSWORD?:}" \
     "${registry}"
-docker run --rm --privileged docker/binfmt:latest --install linux/arm64,linux/amd64
-RUN docker buildx create --name simx-builder-multi
-RUN docker buildx use simx-builder-multi
-RUN docker buildx inspect --bootstrap
+docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad6 --install linux/arm64,linux/amd64
+docker buildx create --name simx-builder-multi
+docker buildx use simx-builder-multi
+docker buildx inspect --bootstrap
 docker buildx build --platform linux/arm64,linux/amd64 -t "${image}" .
 docker logout
 docker login \
