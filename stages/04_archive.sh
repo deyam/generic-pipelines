edb26446adb850a6bc5cf55cc2677eaa7309d1f5
Deya Motawie <deya787@gmail.com>
2021-03-19 16:10:05 +0000

    changed errors in 04

--- stages/04_archive.sh
+++ stages/04_archive.sh
@@ -11,7 +11,8 @@ docker login \
     -u "${DOCKER_USERNAME?:}" \
     -p "${DOCKER_PASSWORD?:}" \
     "${registry}"
-docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad6 --install linux/arm64,linux/amd64
+docker run --rm --privileged docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447ad3 
+#docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad6 --install linux/arm64,linux/amd64
 docker buildx create --name simx-builder-multi
 docker buildx use simx-builder-multi
 docker buildx inspect --bootstrap
