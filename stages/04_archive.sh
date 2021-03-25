9cf4c532c88d065077685c227b792cef5a47ff1e
Deya Motawie <deya787@gmail.com>
2021-03-25 09:20:25 +0200

    version added, using jdk8 with gradle, and fixed versions diff between github and local

--- stages/04_archive.sh
+++ stages/04_archive.sh
@@ -12,17 +12,8 @@ docker login \
     -u "${DOCKER_USERNAME?:}" \
     -p "${DOCKER_PASSWORD?:}" \
     "${registry}"
-<<<<<<< HEAD
-=======
-##docker run -d --name nginx nginx
-##docker run --rm --privileged docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447ad3 
-#docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad6 --install linux/arm64,linux/amd64
-#docker buildx create --name simx-builder-multi
-#docker buildx use simx-builder-multi
-#docker buildx inspect --bootstrap
->>>>>>> 3bff29055116b586e6ea11b5faefb41ec9301b01
 #docker buildx build --platform linux/arm64,linux/amd64 -t "${image}" .
-docker build -t "${image}" .
+docker build -t "${image}:${version}" .
 docker logout
 docker login \
     -u "${DOCKER_USERNAME?:}" \
